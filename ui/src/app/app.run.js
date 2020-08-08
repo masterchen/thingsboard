45358b9abdb72be6ee4095464acf91e016ea8bd2
chenfeng <chenfeng10000@126.com>
2020-08-08 19:38:46 +0800

    提交LOGO 和翻译

--- ui/src/app/app.run.js
+++ ui/src/app/app.run.js
@@ -146,9 +146,9 @@ export default function AppRun($rootScope, $window, $injector, $location, $log, 
             }
             if (angular.isDefined(to.data.pageTitle)) {
                 $translate(to.data.pageTitle).then(function (translation) {
-                    $rootScope.pageTitle = 'ThingsBoard | ' + translation;
+                    $rootScope.pageTitle = 'AnyCon | ' + translation;
                 }, function (translationId) {
-                    $rootScope.pageTitle = 'ThingsBoard | ' + translationId;
+                    $rootScope.pageTitle = 'AnyCon | ' + translationId;
                 });
             }
         })
